cc45ef9135d3dc5c7438bc8ca4c3938daac82ca1
Evan Jones <evanjones125@gmail.com>
2023-04-02 11:00:32 -0400

    create and register location model

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# create models
+class Location(models.Model):
+    title = models.CharField(max_length=120, default='title')
+    latitude = models.DecimalField(max_digits=8, decimal_places=6, default=0.000000)
+    longitude = models.DecimalField(max_digits=8, decimal_places=6, default=0.000000)
+    trip_date = models.DateField()
+
+    def _str_(self):
+        return self.title
