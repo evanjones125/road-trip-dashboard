e2cddd60314a4b1e704554717c19ed2120a15b32
Evan Jones <evanjones125@gmail.com>
2023-04-02 13:27:50 -0400

    set up CRUD functionality on back end

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Location(models.Model):
     title = models.CharField(max_length=120, default='title')
     latitude = models.DecimalField(max_digits=8, decimal_places=6, default=0.000000)
-    longitude = models.DecimalField(max_digits=8, decimal_places=6, default=0.000000)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, default=0.000000)
     trip_date = models.DateField()
 
     def _str_(self):
