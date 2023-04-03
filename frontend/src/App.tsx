00b1c2399f23d7895ff03b4a946655952d54f60f
Evan Jones <evanjones125@gmail.com>
2023-04-03 13:32:25 -0400

    create boilerplate for App.tsx functions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,83 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
-  // http://www.udottraffic.utah.gov/1_devices/aux17859.jpeg?rand=0.9856554651444871
+  // const [cameras, setCameras] = useState([]);
+
+  const cameras = [
+    {
+      "id": 1,
+      "name": "SR12 @ Milepost 97.28 GA",
+      "agency": "UDOT",
+      "latitude": "38.011700",
+      "longitude": "-111.370485",
+      "url": "http://udottraffic.utah.gov/1_devices/SR-12-MP-97.gif"
+    },
+    {
+      "id": 2,
+      "name": "SR12 @ GAWE County Line MP 109.84 GA",
+      "agency": "UDOT",
+      "latitude": "38.147209",
+      "longitude": "-111.326950",
+      "url": "http://udottraffic.utah.gov/1_devices/SR-12-MP-109.gif?rand=0.7404116890134775"
+    },
+    {
+      "id": 3,
+      "name": "US191 Liveview SB @ SR46 La Sal Jct MP 103.55 SJ",
+      "agency": "UDOT",
+      "latitude": "38.309937",
+      "longitude": "-109.404781",
+      "url": "http://udottraffic.utah.gov/1_devices/us-191-mp-103.gif"
+    },
+    {
+      "id": 4,
+      "name": "Main St SR63 @ Center St BCC",
+      "agency": "UDOT",
+      "latitude": "37.674056",
+      "longitude": "-112.156216",
+      "url": "http://udottraffic.utah.gov/1_devices/aux16683.jpeg"
+    },
+    {
+      "id": 5,
+      "name": "US191 NB @ Arches Entrance Rd MP 130.28 GR",
+      "agency": "UDOT",
+      "latitude": "38.611130",
+      "longitude": "-109.608187",
+      "url": "http://udottraffic.utah.gov/1_devices/aux16852.jpeg?rand=0.7026757689401169"
+    }
+  ]
+
+  const locations = [
+    {
+      "id": 2,
+      "title": "Elephant Butte",
+      "latitude": "38.696597",
+      "longitude": "-109.540064",
+      "trip_date": "2023-04-09"
+    },
+    {
+      "id": 3,
+      "title": "Bryce Canyon",
+      "latitude": "37.628186",
+      "longitude": "-112.167648",
+      "trip_date": "2023-04-11"
+    }
+  ]
+
+  const findNearestCamera = () => {
+    
+  }
+
+  const renderLocations = () => {
+    return locations.map(location => {
+      return (
+        <div style={{borderBottom: '1px solid white'}}>
+          <p>Location: {location.title}</p>
+          <p>Trip date: {location.trip_date}</p>
+          <img src={'http://udottraffic.utah.gov/1_devices/SR-56-MP0-53.gif'}/>
+        </div>
+      )
+    })
+  }
 
   return (
     <div className="App">
@@ -17,19 +93,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <img src={'http://udottraffic.utah.gov/1_devices/SR-56-MP0-53.gif'} />
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h1>Locations:</h1>
+      <ul>
+        {renderLocations()}
+      </ul>
+
     </div>
   )
 }
