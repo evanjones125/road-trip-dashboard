fd36187bda7ed94db7aa533b284132f850160522
Evan Jones <evanjones125@gmail.com>
2023-04-07 15:18:40 -0400

    configure prettier and eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css'
+import './App.css';
 
 function App() {
   const [cameras, setCameras] = useState<any[]>([]);
@@ -8,63 +8,63 @@ function App() {
 
   const camerasArr = [
     {
-      "id": 1,
-      "name": "SR12 @ Milepost 97.28 GA",
-      "agency": "UDOT",
-      "latitude": "38.011700",
-      "longitude": "-111.370485",
-      "url": "http://udottraffic.utah.gov/1_devices/SR-12-MP-97.gif"
+      id: 1,
+      name: 'SR12 @ Milepost 97.28 GA',
+      agency: 'UDOT',
+      latitude: '38.011700',
+      longitude: '-111.370485',
+      url: 'http://udottraffic.utah.gov/1_devices/SR-12-MP-97.gif',
     },
     {
-      "id": 2,
-      "name": "SR12 @ GAWE County Line MP 109.84 GA",
-      "agency": "UDOT",
-      "latitude": "38.147209",
-      "longitude": "-111.326950",
-      "url": "http://udottraffic.utah.gov/1_devices/SR-12-MP-109.gif?rand=0.7404116890134775"
+      id: 2,
+      name: 'SR12 @ GAWE County Line MP 109.84 GA',
+      agency: 'UDOT',
+      latitude: '38.147209',
+      longitude: '-111.326950',
+      url: 'http://udottraffic.utah.gov/1_devices/SR-12-MP-109.gif?rand=0.7404116890134775',
     },
     {
-      "id": 3,
-      "name": "US191 Liveview SB @ SR46 La Sal Jct MP 103.55 SJ",
-      "agency": "UDOT",
-      "latitude": "38.309937",
-      "longitude": "-109.404781",
-      "url": "http://udottraffic.utah.gov/1_devices/us-191-mp-103.gif"
+      id: 3,
+      name: 'US191 Liveview SB @ SR46 La Sal Jct MP 103.55 SJ',
+      agency: 'UDOT',
+      latitude: '38.309937',
+      longitude: '-109.404781',
+      url: 'http://udottraffic.utah.gov/1_devices/us-191-mp-103.gif',
     },
     {
-      "id": 4,
-      "name": "Main St SR63 @ Center St BCC",
-      "agency": "UDOT",
-      "latitude": "37.674056",
-      "longitude": "-112.156216",
-      "url": "http://udottraffic.utah.gov/1_devices/aux16683.jpeg"
+      id: 4,
+      name: 'Main St SR63 @ Center St BCC',
+      agency: 'UDOT',
+      latitude: '37.674056',
+      longitude: '-112.156216',
+      url: 'http://udottraffic.utah.gov/1_devices/aux16683.jpeg',
     },
     {
-      "id": 5,
-      "name": "US191 NB @ Arches Entrance Rd MP 130.28 GR",
-      "agency": "UDOT",
-      "latitude": "38.611130",
-      "longitude": "-109.608187",
-      "url": "http://udottraffic.utah.gov/1_devices/aux16852.jpeg?rand=0.7026757689401169"
-    }
-  ]
+      id: 5,
+      name: 'US191 NB @ Arches Entrance Rd MP 130.28 GR',
+      agency: 'UDOT',
+      latitude: '38.611130',
+      longitude: '-109.608187',
+      url: 'http://udottraffic.utah.gov/1_devices/aux16852.jpeg?rand=0.7026757689401169',
+    },
+  ];
 
   const locationsArr = [
     {
-      "id": 2,
-      "title": "Elephant Butte",
-      "latitude": "38.696597",
-      "longitude": "-109.540064",
-      "trip_date": "2023-04-09"
+      id: 2,
+      title: 'Elephant Butte',
+      latitude: '38.696597',
+      longitude: '-109.540064',
+      trip_date: '2023-04-09',
     },
     {
-      "id": 3,
-      "title": "Bryce Canyon",
-      "latitude": "37.628186",
-      "longitude": "-112.167648",
-      "trip_date": "2023-04-11"
-    }
-  ]
+      id: 3,
+      title: 'Bryce Canyon',
+      latitude: '37.628186',
+      longitude: '-112.167648',
+      trip_date: '2023-04-11',
+    },
+  ];
 
   useEffect(() => {
     axios
@@ -76,29 +76,26 @@ function App() {
   console.log(cameras);
 
   const renderLocations = () => {
-    return locationsArr.map(location => {
+    return locationsArr.map((location) => {
       const id = location.id;
-      
+
       return (
         <div>
           <p>Location: {location.title}</p>
           <p>Trip date: {location.trip_date}</p>
-          <img src={camerasArr[id - 1].url}/>
+          <img src={camerasArr[id - 1].url} />
           <p>test</p>
         </div>
-      )
-    })
-  }
+      );
+    });
+  };
 
   return (
     <div>
       <h1>Locations:</h1>
-      <ul>
-        {renderLocations()}
-      </ul>
-
+      <ul>{renderLocations()}</ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
