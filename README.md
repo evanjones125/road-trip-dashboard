95c8f6cf6b15b612b3a29a9501f34188ae9bba65
Evan Jones <evanjones125@gmail.com>
2023-04-10 09:26:47 -0400

    commit for merge into main branch

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # trip-dashboard
 
 enter the desination of a trip and the dashboard will display the nearest traffic road cam and the weather of the nearest town, saving the user time of searching that information separately
+
+- show weather data from the NWS API
+- show sunrise/sunset times
+- show moonrise/moonset times
+  - tell the user what time window they'll have to view the sky after sunset when there's no moon in the sky
+- show milky way information
--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Input.css';
 
 const Input: React.FC = (): JSX.Element => {
+  const [count, setCount] = useState(0);
+
+  const handleSubmit = (): void => {
+    setCount(count + 1);
+    console.log(count);
+  };
+
   return (
     <>
       <h2>Add a trip to your dashboard</h2>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-row">
           <label htmlFor="location">Location</label>
           <input type="text" id="location" name="location"></input>
@@ -14,6 +21,10 @@ const Input: React.FC = (): JSX.Element => {
         <div>
           <label htmlFor="date">Date</label>
           <input type="date" id="date" name="date"></input>
+        </div>
+
+        <div>
+          <input type="submit" value="Add"></input>
         </div>
       </form>
     </>
