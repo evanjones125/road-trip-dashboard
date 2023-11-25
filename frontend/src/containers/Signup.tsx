d27b618586a894f77a04e89fae7a0b0f5fea642c
Evan Jones <evanjones125@gmail.com>
2023-11-24 20:50:56 -0500

    update styling on signup page

--- frontend/src/containers/Signup.tsx
+++ frontend/src/containers/Signup.tsx
@@ -5,6 +5,15 @@ import type { InputEvent } from '../types/types';
 import { userSignup } from '../features/authSlice';
 import type { User } from '../features/authSlice';
 import type { AppDispatch } from '../store';
+import {
+  TextField,
+  Button,
+  Box,
+  Typography,
+  Container,
+  ThemeProvider,
+} from '@mui/material';
+import { createTheme } from '@mui/material/styles';
 
 const Signup = () => {
   const [credentials, setCredentials] = useState<User>({
@@ -29,34 +38,56 @@ const Signup = () => {
 
   return (
     <>
-      <div className="signup-container">
-        <h1>Signup</h1>
-        <form onSubmit={handleSignup}>
-          <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            name="username"
-            type="text"
-            value={username}
-            onChange={handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            value={password}
-            onChange={handleChange}
-          />
-          <button type="submit">Signup</button>
-        </form>
-      </div>
-
-      <div className="login-link">
-        <p>
-          Already have an account? <Link to="/login">Log in here</Link>
-        </p>
-      </div>
+      <ThemeProvider theme={createTheme({ palette: { mode: 'dark' } })}>
+        <Container component="main" maxWidth="xs">
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography variant="h2" component="h2">
+              Sign Up
+            </Typography>
+            <Box component="form" onSubmit={handleSignup}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                name="username"
+                label="Username"
+                variant="outlined"
+                value={username}
+                onChange={handleChange}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                variant="outlined"
+                type="password"
+                value={password}
+                onChange={handleChange}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Link to="/login">Already have an account? sign in here</Link>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
