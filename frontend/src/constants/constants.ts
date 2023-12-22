a0b1b455112d7532cc6e0ffd8d067600113cf086
Evan Jones <evanjones125@gmail.com>
2023-12-21 20:40:37 -0500

    remove trailing slash from base url

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:8000';
-export const BASE_URL = 'https://roadtrip-dashboard-backend.vercel.app/';
+export const BASE_URL = 'https://roadtrip-dashboard-backend.vercel.app';
