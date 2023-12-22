767ec10b7c8756fdcadf741bc94142730d3fe17e
Evan Jones <evanjones125@gmail.com>
2023-12-21 20:36:20 -0500

    change base url

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:8000';
-export const BASE_URL = 'https://trip-dashboard.onrender.com';
+export const BASE_URL = 'https://roadtrip-dashboard-backend.vercel.app/';
