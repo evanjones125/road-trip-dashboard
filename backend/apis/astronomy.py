9e98220b586a47e6a2f5937860addb2939b89c4d
Evan Jones <evanjones125@gmail.com>
2024-01-04 10:38:50 -0500

    plug in milky way data for 2024

--- backend/apis/astronomy.py
+++ backend/apis/astronomy.py
@@ -13,109 +13,429 @@ sun_and_moon_data_cache = {}
 
 # milky way viewing data for the American Southwest from Capture the Atlas (I couldn't find a milky way API)
 milky_way_dates = {
-    "2023-10-07": {
-        "visibility": {
-            "start": "8:32pm",
-            "end": "10:30pm",
-        },
-        "position": "Vertical (85deg) to vertical (-75deg)",
-        "report": "The milky way will be visible for a 1 hour, 58 minute window between 8:32pm and 10:30pm.",
-    },
-    "2023-10-14": {
-        "visibility": {
-            "start": "8:22pm",
-            "end": "10:03pm",
-        },
-        "position": "Vertical (-90deg) to vertical (-75deg)",
-        "report": "The milky way will be visible for a 1 hour, 40 minute window between 8:22pm and 10:03pm.",
-    },
-    "2023-10-21": {
+    "2024-01-06": {
         "visibility": {
             "start": "",
             "end": "",
         },
         "position": "",
-        "report": "The milky way won't be visible on this date",
+        "report": "The milky way won't be visible on this date.",
     },
-    "2023-10-28": {
+    "2024-01-13": {
         "visibility": {
             "start": "",
             "end": "",
         },
         "position": "",
-        "report": "The milky way won't be visible on this date",
+        "report": "The milky way won't be visible on this date.",
     },
-    "2023-11-04": {
+    "2024-01-20": {
         "visibility": {
-            "start": "7:59pm",
-            "end": "8:40pm",
+            "start": "5:46am",
+            "end": "6:08am",
         },
-        "position": "Vertical (-80deg)",
-        "report": "The milky way will be visible for a 41 minute window between 7:59pm and 8:40pm.",
+        "position": "Arch (10deg)",
+        "report": "The milky way will be visible for a 21 minute window between 5:46am and 6:08am. The galactic center will be visible in the same window.",
     },
-    "2023-11-11": {
+    "2024-01-27": {
         "visibility": {
-            "start": "6:53pm",
-            "end": "7:12pm",
+            "start": "5:18am",
+            "end": "6:05am",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 46 minute window between 5:18am and 6:05am.",
+    },
+    "2024-02-03": {
+        "visibility": {
+            "start": "4:51am",
+            "end": "6:00am",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 1 hour, 9 minute window between 4:51am and 6:00am.",
+    },
+    "2024-02-10": {
+        "visibility": {
+            "start": "4:23am",
+            "end": "5:54am",
+        },
+        "position": "Arch (10deg) - Arch (25deg)",
+        "report": "The milky way will be visible for a 1 hour, 30 minute window between 4:23am and 5:54am. The galactic center will be visible in the same window.",
+    },
+    "2024-02-17": {
+        "visibility": {
+            "start": "3:56am",
+            "end": "5:47am",
+        },
+        "position": "Arch (10deg) - Arch (30deg)",
+        "report": "The milky way will be visible for a 1 hour, 51 minute window between 3:56am and 5:47am. The galactic center will be visible in the same window.",
+    },
+    "2024-02-24": {
+        "visibility": {
+            "start": "3:28am",
+            "end": "5:39am",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 2 hour, 11 minute window between 3:28am and 5:39am.",
+    },
+    "2024-03-02": {
+        "visibility": {
+            "start": "3:01am",
+            "end": "5:29am",
         },
         "position": "Vertical (-75deg)",
-        "report": "The milky way will be visible for a 19 minute window between 6:53pm and 7:12pm.",
+        "report": "The milky way will be visible for a 2 hour, 28 minute window between 3:01am and 5:29am.",
     },
-    "2023-11-18": {
+    "2024-03-09": {
+        "visibility": {
+            "start": "3:33am",
+            "end": "6:19am",
+        },
+        "position": "Arch (10deg) - Arch (40deg)",
+        "report": "The milky way will be visible for a 2 hour, 46 minute window between 3:33am and 6:19am. The galactic center will be visible in the same window.",
+    },
+    "2024-03-16": {
+        "visibility": {
+            "start": "3:06am",
+            "end": "6:09am",
+        },
+        "position": "Arch (15deg) - Arch (45deg)",
+        "report": "The milky way will be visible for a 3 hour, 3 minute window between 3:06am and 6:09am. The galactic center will be visible from 3:24am to 6:09am.",
+    },
+    "2024-03-23": {
+        "visibility": {
+            "start": "2:38am",
+            "end": "5:58am",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 3 hour, 20 minute window between 2:38am and 5:58am.",
+    },
+    "2024-03-30": {
+        "visibility": {
+            "start": "2:11am",
+            "end": "5:46am",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 3 hour, 35 minute window between 2:11am and 5:46am.",
+    },
+    "2024-04-06": {
+        "visibility": {
+            "start": "1:43am",
+            "end": "5:34am",
+        },
+        "position": "Arch (10deg) - Arch (55deg)",
+        "report": "The milky way will be visible for a 3 hour, 51 minute window between 1:43am and 5:34am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-04-13": {
+        "visibility": {
+            "start": "1:15am",
+            "end": "5:23am",
+        },
+        "position": "Arch (10deg) - Arch (55deg)",
+        "report": "The milky way will be visible for a 4 hour, 8 minute window between 1:15am and 5:23am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-04-20": {
+        "visibility": {
+            "start": "12:47am",
+            "end": "5:11am",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 4 hour, 24 minute window between 12:47am and 5:11am.",
+    },
+    "2024-04-27": {
+        "visibility": {
+            "start": "12:19am",
+            "end": "5:00am",
+        },
+        "position": "Arch (10deg)",
+        "report": "The milky way will be visible for a 4 hour, 41 minute window between 12:19am and 5:00am. The galactic center will be visible from 12:20am to 12:32am.",
+    },
+    "2024-05-04": {
+        "visibility": {
+            "start": "11:51pm",
+            "end": "4:49am",
+        },
+        "position": "Arch (10deg) - Arch (65deg)",
+        "report": "The milky way will be visible for a 4 hour, 58 minute window between 11:51pm and 4:49am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-05-11": {
+        "visibility": {
+            "start": "11:23pm",
+            "end": "4:39am",
+        },
+        "position": "Arch (10deg) - Arch (70deg)",
+        "report": "The milky way will be visible for a 5 hour, 16 minute window between 11:23pm and 4:39am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-05-18": {
+        "visibility": {
+            "start": "10:55pm",
+            "end": "4:28am",
+        },
+        "position": "Vertical (70deg)",
+        "report": "The milky way will be visible for a 5 hour, 33 minute window between 10:55pm and 4:28am. The galactic center will be visible from 4:10am to 4:31am.",
+    },
+    "2024-05-25": {
+        "visibility": {
+            "start": "10:30pm",
+            "end": "4:24am",
+        },
+        "position": "Arch (10deg)",
+        "report": "The milky way will be visible for a 5 hour, 54 minute window between 10:30pm and 4:24am. The galactic center will be visible from 10:30pm to 11:24pm.",
+    },
+    "2024-06-01": {
+        "visibility": {
+            "start": "10:36pm",
+            "end": "4:19am",
+        },
+        "position": "Arch (15deg) - Vertical (80deg)",
+        "report": "The milky way will be visible for a 5 hour, 43 minute window between 10:36pm and 4:19am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-06-08": {
+        "visibility": {
+            "start": "10:42pm",
+            "end": "4:16am",
+        },
+        "position": "Arch (20deg) - Vertical (85deg)",
+        "report": "The milky way will be visible for a 5 hour, 33 minute window between 10:42pm and 4:16am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-06-15": {
+        "visibility": {
+            "start": "10:48pm",
+            "end": "4:14am",
+        },
+        "position": "Vertical (75deg) - Vertical (-90deg)",
+        "report": "The milky way will be visible for a 5 hour, 26 minute window between 10:48pm and 4:14am. The galactic center will be visible from 2:34am to 4:15am.",
+    },
+    "2024-06-22": {
+        "visibility": {
+            "start": "10:48pm",
+            "end": "4:16am",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 5 hour, 28 minute window between 10:48pm and 4:16am.",
+    },
+    "2024-06-29": {
+        "visibility": {
+            "start": "10:48pm",
+            "end": "4:19am",
+        },
+        "position": "Arch (40deg) - Vertical (75deg)",
+        "report": "The milky way will be visible for a 5 hour, 31 minute window between 10:48pm and 4:19am. The galactic center will be visible from 10:48pm to 1:58am. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-07-06": {
+        "visibility": {
+            "start": "10:48pm",
+            "end": "4:24am",
+        },
+        "position": "Arch (45deg) - Vertical (-75eg)",
+        "report": "The milky way will be visible for a 5 hour, 36 minute window between 10:48pm and 4:24am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-07-13": {
+        "visibility": {
+            "start": "10:40pm",
+            "end": "4:05am",
+        },
+        "position": "Vertical (75deg) - Vertical (-75deg)",
+        "report": "The milky way will be visible for a 5 hour, 25 minute window between 10:40pm and 4:05am. The galactic center will be visible from 12:59am to 4:05am. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-07-20": {
+        "visibility": {
+            "start": "10:32pm",
+            "end": "3:47am",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 5 hour, 15 minute window between 10:32pm and 3:47am.",
+    },
+    "2024-07-27": {
+        "visibility": {
+            "start": "10:25pm",
+            "end": "3:10am",
+        },
+        "position": "Arch (55deg) - Vertical (80deg)",
+        "report": "The milky way will be visible for a 4 hour, 45 minute window between 10:25pm and 3:10am. The galactic center will be visible from 10:25pm to 12:31am.",
+    },
+    "2024-07-29": {
+        "visibility": {
+            "start": "10:23pm",
+            "end": "3:05am",
+        },
+        "position": "Vertical (60deg) - Vertical (-75deg)",
+        "report": "The milky way will be visible for a 4 hour, 42 minute window between 10:23pm and 3:05am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-08-03": {
+        "visibility": {
+            "start": "10:16pm",
+            "end": "2:43am",
+        },
+        "position": "Vertical (60deg) - Vertical (-75deg)",
+        "report": "The milky way will be visible for a 4 hour, 26 minute window between 10:16pm and 2:43am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-08-10": {
+        "visibility": {
+            "start": "10:08pm",
+            "end": "2:20am",
+        },
+        "position": "Vertical (80deg) - Vertical (-75deg)",
+        "report": "The milky way will be visible for a 4 hour, 12 minute window between 10:08pm and 2:20am. The galactic center will be visible from 10:25pm to 12:31am.",
+    },
+    "2024-08-17": {
+        "visibility": {
+            "start": "9:54m",
+            "end": "1:48am",
+        },
+        "position": "Vertical (80deg) - Vertical (-75deg)",
+        "report": "The milky way will be visible for a 3 hour, 54 minute window between 9:54m and 1:48am.",
+    },
+    "2024-08-24": {
+        "visibility": {
+            "start": "9:43pm",
+            "end": "1:20am",
+        },
+        "position": "Vertical (70deg) - Vertical (85deg)",
+        "report": "The milky way will be visible for a 3 hour, 37 minute window between 9:43pm and 1:20am. The galactic center will be visible from 9:43pm to 11:05pm.",
+    },
+    "2024-08-31": {
+        "visibility": {
+            "start": "9:30pm",
+            "end": "12:50am",
+        },
+        "position": "Vertical (75deg) - Vertical (-75deg)",
+        "report": "The milky way will be visible for a 3 hour, 20 minute window between 9:30pm and 12:50am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-09-07": {
+        "visibility": {
+            "start": "9:15pm",
+            "end": "12:20am",
+        },
+        "position": "Vertical (75deg) - Vertical (-75deg)",
+        "report": "The milky way will be visible for a 3 hour, 5 minute window between 9:15pm and 12:20am. The galactic center will be visible in the same window. This will be one of the best nights to view the milky way in 2024!",
+    },
+    "2024-09-14": {
+        "visibility": {
+            "start": "9:07pm",
+            "end": "11:58pm",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 2 hour, 51 minute window between 9:07pm and 11:58pm.",
+    },
+    "2024-09-21": {
+        "visibility": {
+            "start": "8:57pm",
+            "end": "11:37pm",
+        },
+        "position": "Vertical (80deg)",
+        "report": "The milky way will be visible for a 2 hour, 40 minute window between 8:57pm and 11:37pm. The galactic center will be visible from 8:57pm to 9:39pm.",
+    },
+    "2024-09-28": {
+        "visibility": {
+            "start": "8:44pm",
+            "end": "11:03pm",
+        },
+        "position": "Vertical (85deg) - Vertical (-75deg)",
+        "report": "The milky way will be visible for a 2 hour, 19 minute window between 8:44pm and 11:03pm. The galactic center will be visible in the same window.",
+    },
+    "2024-10-05": {
+        "visibility": {
+            "start": "8:30pm",
+            "end": "10:28pm",
+        },
+        "position": "Vertical (85deg) - Vertical (-75deg)",
+        "report": "The milky way will be visible for a 1 hour, 58 minute window between 8:30pm and 10:28pm. The galactic center will be visible in the same window.",
+    },
+    "2024-10-12": {
+        "visibility": {
+            "start": "8:24pm",
+            "end": "10:07pm",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 1 hour, 43 minute window between 8:24pm and 10:07pm.",
+    },
+    "2024-10-19": {
+        "visibility": {
+            "start": "8:16pm",
+            "end": "9:47pm",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 1 hour, 31 minute window between 8:16pm and 9:47pm.",
+    },
+    "2024-10-26": {
+        "visibility": {
+            "start": "8:07pm",
+            "end": "9:12pm",
+        },
+        "position": "Vertical (-85deg)",
+        "report": "The milky way will be visible for a 1 hour, 5 minute window between 8:07pm and 9:12pm. The galactic center will be visible in the same window.",
+    },
+    "2024-11-02": {
+        "visibility": {
+            "start": "7:58pm",
+            "end": "8:37pm",
+        },
+        "position": "Vertical (-80deg)",
+        "report": "The milky way will be visible for a 39 minute window between 7:58pm and 8:37pm. The galactic center will be visible in the same window.",
+    },
+    "2024-11-09": {
+        "visibility": {
+            "start": "6:54pm",
+            "end": "7:17pm",
+        },
+        "position": "",
+        "report": "The milky way will be visible for a 23 minute window between 6:54pm and 7:17pm.",
+    },
+    "2024-11-16": {
         "visibility": {
             "start": "",
             "end": "",
         },
         "position": "",
-        "report": "The milky way won't be visible on this date",
+        "report": "The milky way won't be visible on this date.",
     },
-    "2023-11-25": {
+    "2024-11-23": {
         "visibility": {
             "start": "",
             "end": "",
         },
         "position": "",
-        "report": "The milky way won't be visible on this date",
+        "report": "The milky way won't be visible on this date.",
     },
-    "2023-12-02": {
+    "2024-11-30": {
         "visibility": {
             "start": "",
             "end": "",
         },
         "position": "",
-        "report": "The milky way won't be visible on this date",
+        "report": "The milky way won't be visible on this date.",
     },
-    "2023-12-09": {
+    "2024-12-07": {
         "visibility": {
             "start": "",
             "end": "",
         },
         "position": "",
-        "report": "The milky way won't be visible on this date",
+        "report": "The milky way won't be visible on this date.",
     },
-    "2023-12-16": {
+    "2024-12-14": {
         "visibility": {
             "start": "",
             "end": "",
         },
         "position": "",
-        "report": "The milky way won't be visible on this date",
+        "report": "The milky way won't be visible on this date.",
     },
-    "2023-12-23": {
+    "2024-12-21": {
         "visibility": {
             "start": "",
             "end": "",
         },
         "position": "",
-        "report": "The milky way won't be visible on this date",
+        "report": "The milky way won't be visible on this date.",
     },
-    "2023-12-30": {
+    "2024-12-28": {
         "visibility": {
             "start": "",
             "end": "",
         },
         "position": "",
-        "report": "The milky way won't be visible on this date",
+        "report": "The milky way won't be visible on this date.",
     },
 }
 
@@ -183,17 +503,6 @@ def get_sun_and_moon_data(lat: str, lon: str, date: str) -> dict:
         "long": lon,
         "date": date,
     }
-
-    # # make our requests to the geolocation API
-    # try:
-    #     sun_and_moon_data = requests.get(API_BASE_URL, params=params).json()
-    #     params["date"] = (
-    #         datetime.strptime(date, "%Y-%m-%d") + timedelta(days=1)
-    #     ).strftime("%Y-%m-%d")
-    #     next_day_data = requests.get(API_BASE_URL, params=params).json()
-    #     api_requests += 1
-    # except requests.RequestException as e:
-    #     return {"error": f"Failed to fetch data: {e}"}
 
     # make our requests to the geolocation API
     try:
